b26d0d7fe75c5029543b87de90ed28eed9bc98d7
Codemzy <codemzy@gmail.com>
2020-10-16 10:34:58 +0000

    Completed exercise 4 - extra 2

--- src/exercise/04.js
+++ src/exercise/04.js
@@ -2,13 +2,11 @@
 // http://localhost:3000/isolated/exercise/04.js
 
 import React from 'react'
+import {useLocalStorageState} from '../utils'; // extra 2
 
 function Board() {
   // 🐨 squares is the state for this component. Add useState for squares
-  const [squares, setSquares] = React.useState(() => {
-      const storedGame = window.localStorage.getItem("squares"); // extra 1
-      return storedGame ? JSON.parse(storedGame) : Array(9).fill(null);
-  });
+  const [squares, setSquares] = useLocalStorageState("squares", Array(9).fill(null));
 
   // 🐨 We'll need the following bits of derived state:
   // - nextValue ('X' or 'O')
@@ -19,11 +17,6 @@ function Board() {
   const nextValue = calculateNextValue(squares);
   const winner = calculateWinner(squares);
   const status = calculateStatus(winner, squares, nextValue);
-
-  // extra 1
-  React.useEffect(() => {
-    window.localStorage.setItem("squares", JSON.stringify(squares));
-  }, [squares]);
   
   // This is the function your square click handler will call. `square` should
   // be an index. So if they click the center square, this will be `4`.
