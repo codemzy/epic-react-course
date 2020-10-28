63e2e7a7c571ff66ab0c58277599430ada04a40e
Codemzy <codemzy@gmail.com>
2020-10-28 11:20:52 +0000

    Completed exercise 1 - extra 2

--- src/exercise/01.js
+++ src/exercise/01.js
@@ -4,20 +4,34 @@
 import React from 'react'
 
 function Counter({initialCount = 0, step = 1}) {
-  // 🐨 replace React.useState with React.useReducer.
-  // 💰 React.useReducer(countReducer, initialCount)
-  // const [count, setCount] = React.useState(initialCount)
-  const [count, changeCount] = React.useReducer(countReducer, initialCount);
+//   // 🐨 replace React.useState with React.useReducer.
+//   // 💰 React.useReducer(countReducer, initialCount)
+//   // const [count, setCount] = React.useState(initialCount)
+//   const [count, changeCount] = React.useReducer(countReducer, initialCount);
 
-  function countReducer(state, step) {
-    return state + step; 
-  };
-  // 💰 you can write the countReducer function so you don't have to make any
-  // changes to the next two lines of code! Remember:
-  // The 1st argument is called "state" - the current value of count
-  // The 2nd argument is called "newState" - the value passed to setCount
-  const increment = () => changeCount(step)
-  return <button onClick={increment}>{count}</button>
+//   function countReducer(state, step) {
+//     return state + step; 
+//   };
+  
+//   // 💰 you can write the countReducer function so you don't have to make any
+//   // changes to the next two lines of code! Remember:
+//   // The 1st argument is called "state" - the current value of count
+//   // The 2nd argument is called "newState" - the value passed to setCount
+//   const increment = () => changeCount(step)
+
+    // extra 2
+    const [state, setState] = React.useReducer(countReducer, {
+        count: initialCount,
+    });
+
+    function countReducer(state, action) {
+        return {...state, ...action}; 
+    };
+
+    const {count} = state;
+    const increment = () => setState({count: count + step})
+
+    return <button onClick={increment}>{count}</button>
 }
 
 function App() {
