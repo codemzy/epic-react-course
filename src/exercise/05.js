867006d197e85f40e5e6b4d13148e3c956192c24
Codemzy <codemzy@gmail.com>
2020-11-11 15:45:56 +0000

    Completed exercise 5 - state reducer - extra 1

--- src/exercise/05.js
+++ src/exercise/05.js
@@ -60,49 +60,56 @@ function App() {
   const [timesClicked, setTimesClicked] = React.useState(0)
   const clickedTooMuch = timesClicked >= 4
 
-  function toggleStateReducer(state, action) {
-    switch (action.type) {
-      case 'toggle': {
-        if (clickedTooMuch) {
-          return {on: state.on}
+//   function toggleStateReducer(state, action) {
+//     switch (action.type) {
+//       case 'toggle': {
+//         if (clickedTooMuch) {
+//           return {on: state.on}
+//         }
+//         return {on: !state.on}
+//       }
+//       case 'reset': {
+//         return {on: false}
+//       }
+//       default: {
+//         throw new Error(`Unsupported type: ${action.type}`)
+//       }
+//     }
+//   }
+
+    function toggleStateReducer(state, action) {
+        if (action.type === 'toggle' && timesClicked >= 4) {
+            return {on: state.on}
         }
-        return {on: !state.on}
-      }
-      case 'reset': {
-        return {on: false}
-      }
-      default: {
-        throw new Error(`Unsupported type: ${action.type}`)
-      }
-    }
-  }
+        return toggleReducer(state, action)
+    };
 
-  const {on, getTogglerProps, getResetterProps} = useToggle({
-    reducer: toggleStateReducer,
-  })
+    const {on, getTogglerProps, getResetterProps} = useToggle({
+        reducer: toggleStateReducer,
+    })
 
-  return (
-    <div>
-      <Switch
-        {...getTogglerProps({
-          disabled: clickedTooMuch,
-          on: on,
-          onClick: () => setTimesClicked(count => count + 1),
-        })}
-      />
-      {clickedTooMuch ? (
-        <div data-testid="notice">
-          Whoa, you clicked too much!
-          <br />
+    return (
+        <div>
+            <Switch
+                {...getTogglerProps({
+                disabled: clickedTooMuch,
+                on: on,
+                onClick: () => setTimesClicked(count => count + 1),
+                })}
+            />
+            {clickedTooMuch ? (
+                <div data-testid="notice">
+                Whoa, you clicked too much!
+                <br />
+                </div>
+            ) : timesClicked > 0 ? (
+                <div data-testid="click-count">Click count: {timesClicked}</div>
+            ) : null}
+            <button {...getResetterProps({onClick: () => setTimesClicked(0)})}>
+                Reset
+            </button>
         </div>
-      ) : timesClicked > 0 ? (
-        <div data-testid="click-count">Click count: {timesClicked}</div>
-      ) : null}
-      <button {...getResetterProps({onClick: () => setTimesClicked(0)})}>
-        Reset
-      </button>
-    </div>
-  )
+    )
 }
 
 export default App
