4e404f2c82c9f3c68ababa6543f888705ba6bdcf
Codemzy <codemzy@gmail.com>
2020-11-11 14:36:54 +0000

    Completed exercise 4 - prop collections and getters - extra 1

--- src/exercise/04.js
+++ src/exercise/04.js
@@ -4,6 +4,15 @@
 import * as React from 'react'
 import {Switch} from '../switch'
 
+// this is a function that calls all the functions passed to it
+const callAllFunctions = function(...functions) {
+    return function(...args) {
+        functions.forEach(function(funct) {
+            funct?.(...args);
+        })
+    }
+};
+
 function useToggle() {
   const [on, setOn] = React.useState(false)
   const toggle = () => setOn(!on)
@@ -11,22 +20,51 @@ function useToggle() {
   // 🐨 Add a property called `togglerProps`. It should be an object that has
   // `aria-pressed` and `onClick` properties.
   // 💰 {'aria-pressed': on, onClick: toggle}
-  const togglerProps = {'aria-pressed': on, onClick: toggle};
-  return {on, toggle, togglerProps}
+  const togglerProps = {'aria-pressed': on};
+
+  // extra 1
+  const getTogglerProps = function({onClick, ...options}) {
+        return {
+            ...togglerProps,
+            onClick: callAllFunctions(toggle, onClick),
+            ...options
+        }
+  };
+  return {on, toggle, getTogglerProps};
 }
 
+// function App() {
+//   const {on, togglerProps} = useToggle()
+//   return (
+//     <div>
+//       <Switch on={on} {...togglerProps} />
+//       <hr />
+//       <button aria-label="custom-button" {...togglerProps}>
+//         {on ? 'on' : 'off'}
+//       </button>
+//     </div>
+//   )
+// }
+
+// extra 1
 function App() {
-  const {on, togglerProps} = useToggle()
+  const {on, getTogglerProps} = useToggle()
   return (
     <div>
-      <Switch on={on} {...togglerProps} />
+      <Switch {...getTogglerProps({on})} />
       <hr />
-      <button aria-label="custom-button" {...togglerProps}>
+      <button
+        {...getTogglerProps({
+          'aria-label': 'custom-button',
+          onClick: () => console.info('onButtonClick'),
+          id: 'custom-button-id',
+        })}
+      >
         {on ? 'on' : 'off'}
       </button>
     </div>
   )
-}
+};
 
 export default App
 
