8e07ec16500d8dc586f3e2823c5f54928a5ef255
Codemzy <codemzy@gmail.com>
2020-12-09 15:13:33 +0000

    Completed exercise 1

--- src/__tests__/exercise/01.js
+++ src/__tests__/exercise/01.js
@@ -7,23 +7,30 @@ import Counter from '../../components/counter'
 
 test('counter increments and decrements when the buttons are clicked', () => {
   // 🐨 create a div to render your component to (💰 document.createElement)
-  //
+  let div = document.createElement("div");
   // 🐨 append the div to document.body (💰 document.body.append)
-  //
+  document.body.append(div);
   // 🐨 use ReactDOM.render to render the <Counter /> to the div
+  ReactDOM.render(<Counter />, div);
   // 🐨 get a reference to the increment and decrement buttons:
   //   💰 div.querySelectorAll('button')
+  let [decrement, increment] = div.querySelectorAll('button');
   // 🐨 get a reference to the message div:
   //   💰 div.firstChild.querySelector('div')
-  //
+  let message = div.firstChild.querySelector('div');
   // 🐨 expect the message.textContent toBe 'Current count: 0'
+  expect(message.textContent).toBe('Current count: 0');
   // 🐨 click the increment button (💰 increment.click())
+  increment.click();
   // 🐨 assert the message.textContent
+  expect(message.textContent).toBe('Current count: 1');
   // 🐨 click the decrement button (💰 decrement.click())
+  decrement.click();
   // 🐨 assert the message.textContent
-  //
+  expect(message.textContent).toBe('Current count: 0');
   // 🐨 cleanup by removing the div from the page (💰 div.remove())
   // 🦉 If you don't cleanup, then it could impact other tests and/or cause a memory leak
+  div.remove();
 })
 
 /* eslint no-unused-vars:0 */
