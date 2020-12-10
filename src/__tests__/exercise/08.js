24115a187e9ffbce2eb07c55d31c8f5386ce0ba7
Codemzy <codemzy@gmail.com>
2020-12-10 15:58:53 +0000

    Completed exercise 8 - extra 1

--- src/__tests__/exercise/08.js
+++ src/__tests__/exercise/08.js
@@ -2,7 +2,7 @@
 // http://localhost:3000/counter-hook
 
 import * as React from 'react'
-import {render, screen} from '@testing-library/react'
+import {render, screen, act} from '@testing-library/react'
 import userEvent from '@testing-library/user-event'
 import useCounter from '../../components/use-counter'
 
@@ -36,6 +36,21 @@ test('exposes the count and increment/decrement functions', () => {
   expect(message).toHaveTextContent('Count: 1');
   userEvent.click(decrementButton);
   expect(message).toHaveTextContent('Count: 0');
+
+  // extra 1 - a different way of doing the same above tests without the custom Count component
+  // instead use a fake component
+  let result
+  function TestComponent(props) {
+    result = useCounter(props)
+    return null; // so that its a valid component but wont send anything to screen
+  }
+  render(<TestComponent />);
+  // console.log(result);
+  expect(result.count).toBe(0);
+  act(() => {result.increment()});
+  expect(result.count).toBe(1);
+  act(() => {result.decrement()});
+  expect(result.count).toBe(0);
 })
 
 /* eslint no-unused-vars:0 */
