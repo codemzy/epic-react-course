44a4097180a495e59fb54f378651240ac83a0bdd
Codemzy <codemzy@gmail.com>
2020-12-10 15:15:53 +0000

    Completed exercise 7 - extra 2

--- src/__tests__/exercise/07.js
+++ src/__tests__/exercise/07.js
@@ -6,9 +6,18 @@ import {render, screen} from '@testing-library/react'
 import {ThemeProvider} from '../../components/theme'
 import EasyButton from '../../components/easy-button'
 
+// extra 2
+function renderWithTheme(theme) {
+    function Wrapper({children}) {
+        return <ThemeProvider initialTheme={theme}>{children}</ThemeProvider>
+    };
+    return render(<EasyButton>Easy</EasyButton>, {wrapper: Wrapper});
+};
+
 test('renders with the light styles for the light theme', () => {
   // 🐨 uncomment all of this code and your test will be busted on the next line:
-  render(<EasyButton>Easy</EasyButton>, {wrapper: Wrapper})
+  // render(<EasyButton>Easy</EasyButton>, {wrapper: Wrapper})
+  renderWithTheme('light'); // extra 2
   const button = screen.getByRole('button', {name: /easy/i})
   expect(button).toHaveStyle(`
     background-color: white;
@@ -17,15 +26,17 @@ test('renders with the light styles for the light theme', () => {
   //
   // 🐨 update the `render` call above to use the wrapper option using the
   // ThemeProvider
-    function Wrapper({children}) {
-        return <ThemeProvider initialTheme={'light'}>{children}</ThemeProvider>
-    };
+    // function Wrapper({children}) {
+    //     return <ThemeProvider initialTheme={'light'}>{children}</ThemeProvider>
+    // };
 
 })
 
+// extra 1
 test('renders with the dark styles for the dark theme', () => {
   // 🐨 uncomment all of this code and your test will be busted on the next line:
-  render(<EasyButton>Easy</EasyButton>, {wrapper: Wrapper})
+  // render(<EasyButton>Easy</EasyButton>, {wrapper: Wrapper})
+  renderWithTheme('dark'); // extra 2
   const button = screen.getByRole('button', {name: /easy/i})
   expect(button).toHaveStyle(`
     background-color: black;
@@ -34,9 +45,9 @@ test('renders with the dark styles for the dark theme', () => {
   //
   // 🐨 update the `render` call above to use the wrapper option using the
   // ThemeProvider
-    function Wrapper({children}) {
-        return <ThemeProvider initialTheme={'dark'}>{children}</ThemeProvider>
-    };
+    // function Wrapper({children}) {
+    //     return <ThemeProvider initialTheme={'dark'}>{children}</ThemeProvider>
+    // };
 
 })
 
