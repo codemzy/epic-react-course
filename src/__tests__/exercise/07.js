d1a5472a0b5ac24805ba0f627e08c4cd01d73f07
Codemzy <codemzy@gmail.com>
2020-12-10 15:20:00 +0000

    Completed exercise 7 - extra 2 - made the custom render function better so that it can be used with different ui's and options

--- src/__tests__/exercise/07.js
+++ src/__tests__/exercise/07.js
@@ -7,17 +7,17 @@ import {ThemeProvider} from '../../components/theme'
 import EasyButton from '../../components/easy-button'
 
 // extra 2
-function renderWithTheme(theme) {
+function renderWithTheme(ui, { theme = 'light', ...options } = {}) { // so we can wrap theme context around any ui
     function Wrapper({children}) {
         return <ThemeProvider initialTheme={theme}>{children}</ThemeProvider>
     };
-    return render(<EasyButton>Easy</EasyButton>, {wrapper: Wrapper});
+    return render(ui, {wrapper: Wrapper, ...options}); // so we can add other options if we want to
 };
 
 test('renders with the light styles for the light theme', () => {
   // 🐨 uncomment all of this code and your test will be busted on the next line:
   // render(<EasyButton>Easy</EasyButton>, {wrapper: Wrapper})
-  renderWithTheme('light'); // extra 2
+  renderWithTheme(<EasyButton>Easy</EasyButton>); // extra 2
   const button = screen.getByRole('button', {name: /easy/i})
   expect(button).toHaveStyle(`
     background-color: white;
@@ -36,7 +36,7 @@ test('renders with the light styles for the light theme', () => {
 test('renders with the dark styles for the dark theme', () => {
   // 🐨 uncomment all of this code and your test will be busted on the next line:
   // render(<EasyButton>Easy</EasyButton>, {wrapper: Wrapper})
-  renderWithTheme('dark'); // extra 2
+  renderWithTheme(<EasyButton>Easy</EasyButton>, { theme: 'dark' }); // extra 2
   const button = screen.getByRole('button', {name: /easy/i})
   expect(button).toHaveStyle(`
     background-color: black;
