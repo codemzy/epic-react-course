adf66d36b11b449d99d39eee83e8ee9d67823b5d
Codemzy <codemzy@gmail.com>
2020-12-10 15:10:33 +0000

    Completed exercise 7 - extra 1

--- src/__tests__/exercise/07.js
+++ src/__tests__/exercise/07.js
@@ -18,7 +18,24 @@ test('renders with the light styles for the light theme', () => {
   // 🐨 update the `render` call above to use the wrapper option using the
   // ThemeProvider
     function Wrapper({children}) {
-        return <ThemeProvider>{children}</ThemeProvider>
+        return <ThemeProvider initialTheme={'light'}>{children}</ThemeProvider>
+    };
+
+})
+
+test('renders with the dark styles for the dark theme', () => {
+  // 🐨 uncomment all of this code and your test will be busted on the next line:
+  render(<EasyButton>Easy</EasyButton>, {wrapper: Wrapper})
+  const button = screen.getByRole('button', {name: /easy/i})
+  expect(button).toHaveStyle(`
+    background-color: black;
+    color: white;
+  `)
+  //
+  // 🐨 update the `render` call above to use the wrapper option using the
+  // ThemeProvider
+    function Wrapper({children}) {
+        return <ThemeProvider initialTheme={'dark'}>{children}</ThemeProvider>
     };
 
 })
