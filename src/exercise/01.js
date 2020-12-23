60d653447500a0d7c73194c58d4ed0dd295ea912
Codemzy <codemzy@gmail.com>
2020-12-23 12:00:00 +0000

    Completed exercise 1 - extra 2 - added generic code for the promise handling

--- src/exercise/01.js
+++ src/exercise/01.js
@@ -7,9 +7,9 @@ import {PokemonDataView, fetchPokemon, PokemonErrorBoundary} from '../pokemon'
 
 // 💰 use it like this: fetchPokemon(pokemonName).then(handleSuccess, handleFailure)
 
-// 🐨 create a variable called "pokemon" (using let)
-let pokemon;
-let pokemonError; // extra 1
+// // 🐨 create a variable called "pokemon" (using let)
+// let pokemon;
+// let pokemonError; // extra 1
 // 💣 delete this now...
 // const pokemon = {
 //   name: 'TODO',
@@ -20,28 +20,53 @@ let pokemonError; // extra 1
 //   fetchedAt: 'TODO',
 // }
 
-// We don't need the app to be mounted to know that we want to fetch the pokemon
-// named "pikachu" so we can go ahead and do that right here.
-// 🐨 assign a pokemonPromise variable to a call to fetchPokemon('pikachu')
-const pokemonPromise = fetchPokemon('pikachu');
-// 🐨 when the promise resolves, assign the "pokemon" variable to the resolved value
-// 💰 For example: somePromise.then(resolvedValue => (someValue = resolvedValue))
-pokemonPromise.then(function(response) {
-    pokemon = response;
-}).catch(function(error){
-    pokemonError = error; // extra 1
-});
+// extra 2
+function createResource(promise) {
+    let status = "pending";
+    let result = promise.then(function(resolved) {
+        status = "resolved";
+        result = resolved;
+    }).catch(function(error) {
+        status = "rejected";
+        result = error; // extra 1
+    });
+    return {
+        read() {
+            if (status === "pending" || status === "rejected") {
+                throw result; // throw the promise while pending or the error when rejected
+            } else if (status === "resolved") {
+                return result;
+            }
+        }
+    }
+}
+
+// extra 2
+const pokemonResource = createResource(fetchPokemon('pikachu'));
+
+// // We don't need the app to be mounted to know that we want to fetch the pokemon
+// // named "pikachu" so we can go ahead and do that right here.
+// // 🐨 assign a pokemonPromise variable to a call to fetchPokemon('pikachu')
+// const pokemonPromise = fetchPokemon('pikachu');
+// // 🐨 when the promise resolves, assign the "pokemon" variable to the resolved value
+// // 💰 For example: somePromise.then(resolvedValue => (someValue = resolvedValue))
+// pokemonPromise.then(function(response) {
+//     pokemon = response;
+// }).catch(function(error){
+//     pokemonError = error; // extra 1
+// });
 
 function PokemonInfo() {
-  // extra 1 - for error boundary
-  if (pokemonError) {
-      throw pokemonError;
-  }
-  // 🐨 if there's no pokemon yet, then throw the pokemonPromise
-  // 💰 (no, for real. Like: `throw pokemonPromise`)
-  if (!pokemon) {
-      throw pokemonPromise; // so react knows when to re-render (when promise is resolved)
-  }
+//   // extra 1 - for error boundary
+//   if (pokemonError) {
+//       throw pokemonError;
+//   }
+//   // 🐨 if there's no pokemon yet, then throw the pokemonPromise
+//   // 💰 (no, for real. Like: `throw pokemonPromise`)
+//   if (!pokemon) {
+//       throw pokemonPromise; // so react knows when to re-render (when promise is resolved)
+//   }
+  const pokemon = pokemonResource.read();
   // if the code gets it this far, then the pokemon variable is defined and
   // rendering can continue!
   return (
